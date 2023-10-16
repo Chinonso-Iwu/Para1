f50c328fb4d09b59ce7843a0ee3d7b433776b00c
Chinonso-Iwu <143559394+Chinonso-Iwu@users.noreply.github.com>
2023-10-15 23:08:16 -0600

    Add files via upload

---
+++ Paratransit.js
@@ -0,0 +1,20 @@
+function Myfunction(){
+    var a = document.getElementById("button1");
+
+    a.style.display = 'none';
+
+    var b = document.getElementById("div3");
+
+    b.style.display = 'block';
+}
+
+function c(){
+    document.getElementById("P2").innerHTML="Note: Only One Person is allow to Accompany you."
+}
+
+function d(){
+    document.getElementById("P2").innerHTML=""
+}
+
+
+
